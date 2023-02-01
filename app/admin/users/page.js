5313a2c8d731333a7e6ddea57be7534b4adbab20
Tony Kosseify <tonykosseify123@gmail.com>
2023-02-01 02:16:05 +0200

    update: my own table

--- app/admin/users/page.js
+++ app/admin/users/page.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import UsersTable from "../components/Users";
 import styles from "./styles.module.css";
+
+const items = [
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+  ["asdkjasd", "tony kosseify", "tonyasda", "asdasd"],
+]
 
 function Users() {
   return (
     <div className={styles.users_wrapper}>
-      <UsersTable />
+      <div className={`${styles.users_header} ${styles.grid}`}>
+        <div><p>#user_id</p></div>
+        <div><p>full name</p></div>
+        <div><p>email</p></div>
+        <div><p>password</p></div>
+        <div><p>provider</p></div>
+      </div>
+      <div className={styles.users_table}>
+        {items.map((item) => (
+          <div key={item} className={`${styles.grid} ${styles.rows}`}>
+            <div><p>{item[0]}</p></div>
+            <div><p>{item[1]}</p></div>
+            <div><p>email</p></div>
+            <div><p>password</p></div>
+            <div><p>provider</p></div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
